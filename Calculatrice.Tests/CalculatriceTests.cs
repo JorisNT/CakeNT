3d9fa70dcdbb263df3006f800b7015d6fe190d27
JorisNT <jorisnsenguet@gmail.com>
2016-03-21 21:16:21 +0100

    Test + maj appveyor

--- Calculatrice.Tests/CalculatriceTests.cs
+++ Calculatrice.Tests/CalculatriceTests.cs
@@ -1,16 +1,14 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
-
-
 
 namespace Calculatrice.Tests
 {
     [TestFixture]
-    public class CalculatriceTest
+    public class CalculatriceTests
     {
         [Test]
         public void AdditionTest()
--- Calculatrice.Tests/Properties/AssemblyInfo.cs
+++ Calculatrice.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("CalculatriceTests")]
+[assembly: AssemblyTitle("Calculatrice.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalculatriceTests")]
+[assembly: AssemblyProduct("Calculatrice.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("b3b40f89-d1cc-44d4-b4a9-071f20fd8a04")]
+[assembly: Guid("4253112a-569e-48e1-b38b-369b2041f85e")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
