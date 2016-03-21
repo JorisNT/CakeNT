8e639fe010d911dbfbe542ec035ebfe216ae6709
JorisNT <jorisnsenguet@gmail.com>
2016-03-21 16:07:06 +0100

    Init

---
+++ Calculatrice/Calculatrice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice
+{
+    public class Calculatrice
+    {
+        public Calculatrice() { }
+
+        public double Addition(double nbOne, double nbTwo)
+        {
+            return nbOne + nbTwo;
+        }
+
+        public double Soustraction(double nbOne, double nbTwo)
+        {
+            return nbOne - nbTwo;
+        }
+    }
+}
---
+++ CalculatriceTests/CalculatriceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+
+namespace Calculatrice.Tests
+{
+    [TestFixture]
+    public class CalculatriceTest
+    {
+        [Test]
+        public void AdditionTest()
+        {
+            Calculatrice c = new Calculatrice();
+            Assert.AreEqual(5, c.Addition(2, 3));
+        }
+    }
+}
